f9a07e302ad99457103a7dadc7955edaaddc7681
takumahirasaki <hisaishi1summerday@gmail.com>
2024-12-04 08:25:29 +0900

    second

---
+++ src/test/java/com/example/mapdemo/MapdemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.mapdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MapdemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
